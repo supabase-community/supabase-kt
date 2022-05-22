afd8211687cd7906b690e0614328686b77962693
TheRealJan <jan.m.tennert@gmail.com>
2022-05-22 16:51:55 +0200

    Fix android oauth provider implementation

--- Auth/src/androidMain/kotlin/io/github/jan/supacompose/auth/providers/OAuthProvider.kt
+++ Auth/src/androidMain/kotlin/io/github/jan/supacompose/auth/providers/OAuthProvider.kt
@@ -2,23 +2,20 @@ package io.github.jan.supacompose.auth.providers
 
 import io.github.jan.supacompose.auth.user.UserSession
 
-actual open class OAuthProvider : AuthProvider<ExternalAuthConfig, Unit> {
+actual abstract class OAuthProvider : AuthProvider<ExternalAuthConfig, Unit> {
+
+    actual abstract fun provider(): String
 
     actual override suspend fun login(
-        supabaseClient: io.github.jan.supacompose.SupabaseClient,
+        supabaseClient: SupabaseClient,
         onSuccess: suspend (UserSession) -> Unit,
         onFail: (OAuthFail) -> Unit,
         credentials: (ExternalAuthConfig.() -> Unit)?
     ) {
-        TODO("Not yet implemented")
+        TODO()
     }
 
-    actual override suspend fun signUp(
-        supabaseClient: io.github.jan.supacompose.SupabaseClient,
-        credentials: ExternalAuthConfig.() -> Unit
-    ) {
+    actual override suspend fun signUp(supabaseClient: SupabaseClient, credentials: ExternalAuthConfig.() -> Unit) {
         TODO("Not yet implemented")
     }
-
-
 }
