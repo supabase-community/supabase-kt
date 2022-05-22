2a7e31fa92dc78dc8dcf42fff1d02703e2a95134
TheRealJan <jan.m.tennert@gmail.com>
2022-05-22 16:36:44 +0200

    Fix a bug

--- android/src/main/java/io/github/jan/supacompose/android/MainActivity.kt
+++ android/src/main/java/io/github/jan/supacompose/android/MainActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.remember
 import io.github.jan.supacompose.auth.Auth
-import io.github.jan.supacompose.auth.auth
 import io.github.jan.supacompose.createSupabaseClient
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +15,9 @@ class MainActivity : AppCompatActivity() {
             MaterialTheme {
                 val supabaseClient = remember {
                     createSupabaseClient {
-                        supabaseUrl = "https://arnyfaeuskyqfxkvotgj.supabase.co"
-                        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFybnlmYWV1c2t5cWZ4a3ZvdGdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTMwMzkxMTEsImV4cCI6MTk2ODYxNTExMX0.ItmL8lfnOL9oy7CEX9N6TnYt10VVhk-KTlwley4aq1M"
-
+                        supabaseUrl = System.getenv("SUPABASE_URL")
+                        supabaseKey = System.getenv("SUPABASE_KEY")
+                        
                         install(Auth)
                     }
                 }
