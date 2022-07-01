794d0528dde3ffe3d39b5a0613da814962fcff66
TheRealJan <jan.m.tennert@gmail.com>
2022-07-01 20:37:02 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,20 +1,20 @@
 object Versions {
 
     const val KOTLIN = "1.7.0"
-    const val MATERIAL = "1.6.0"
-    const val SERIALIZATION = "1.3.2"
-    const val KTOR = "2.0.2"
+    const val MATERIAL = "1.6.1"
+    const val SERIALIZATION = "1.3.3"
+    const val KTOR = "2.0.3"
     const val AGP = "7.2.0"
     const val ANDROID_CORE = "1.7.0"
-    const val COROUTINES = "1.6.1"
-    const val DOKKA = "1.6.21"
+    const val COROUTINES = "1.6.3"
+    const val DOKKA = "1.7.0"
     const val NEXUS_STAGING = "0.30.0"
-    const val SUPACOMPOSE = "0.0.17"
+    const val SUPACOMPOSE = "0.0.18"
     const val KORLIBS = "2.7.0"
     const val ANDROID_COMPAT = "1.4.1"
     const val COMPOSE = "1.2.0-alpha01-dev716"
     const val ACTIVITY = "1.4.0"
     const val NAPIER = "2.6.1"
-    const val DATETIME = "0.3.2" //don't update to 0.3.3 yet because it uses Kotlin 1.7 Beta
+    const val DATETIME = "0.4.0"
 
 }
