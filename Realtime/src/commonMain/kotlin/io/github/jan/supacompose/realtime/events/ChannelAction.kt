5db9f2173b24c4e2859c12a799bc0f64ea7b4e0b
TheRealJan <jan.m.tennert@gmail.com>
2022-08-12 21:51:49 +0200

    Add an extra function to channel action

--- Realtime/src/commonMain/kotlin/io/github/jan/supacompose/realtime/events/ChannelAction.kt
+++ Realtime/src/commonMain/kotlin/io/github/jan/supacompose/realtime/events/ChannelAction.kt
@@ -3,7 +3,9 @@ package io.github.jan.supacompose.realtime.events
 import kotlinx.datetime.Instant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.decodeFromJsonElement
 
 /**
  * Contains information about a column
@@ -71,3 +73,21 @@ sealed interface ChannelAction {
     ): ChannelAction
 
 }
+
+/**
+ * Decodes [ChannelAction.record] as [T] and returns it or null when either [ChannelAction.record] is null or it cannot be decoded as [T]
+ */
+inline fun <reified T> ChannelAction.decodeRecordOrNull(json: Json = Json): T? {
+    return if(record != null) {
+        try {
+            json.decodeFromJsonElement<T>(record!!)
+        } catch (e: Exception) {
+            null
+        }
+    } else null
+}
+
+/**
+ * Decodes [ChannelAction.oldRecord] as [T] and returns it
+ */
+inline fun <reified T> ChannelAction.decodeRecord(json: Json = Json) = json.decodeFromJsonElement<T>(record!!)
