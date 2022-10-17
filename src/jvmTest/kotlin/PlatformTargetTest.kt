84622502ec78904317bf20bd576ffae6306abfc6
TheRealJan <jan.m.tennert@gmail.com>
2022-10-17 17:32:11 +0200

    Fix build error

---
+++ src/jvmTest/kotlin/PlatformTargetTest.kt
@@ -0,0 +1,13 @@
+import io.github.jan.supabase.CurrentPlatformTarget
+import io.github.jan.supabase.PlatformTarget
+import org.junit.Test
+import kotlin.test.assertEquals
+
+actual class PlatformTargetTest {
+
+    @Test
+    actual fun testPlatformTarget() {
+        assertEquals(PlatformTarget.DESKTOP, CurrentPlatformTarget, "The current platform target should be DESKTOP")
+    }
+
+}
