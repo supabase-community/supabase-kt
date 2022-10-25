0c8d2f82e812510a0d9ab0ff4cf90d06e0338837
TheRealJan <jan.m.tennert@gmail.com>
2022-10-25 15:48:53 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,18 +1,18 @@
 object Versions {
 
-    const val KOTLIN = "1.7.10"
-    const val KTOR = "2.1.1"
+    const val KOTLIN = "1.7.20"
+    const val KTOR = "2.1.2"
     const val ANDROID_CORE = "1.9.0"
     const val COROUTINES = "1.6.4"
     const val DOKKA = "1.7.10"
     const val NEXUS_STAGING = "0.30.0"
-    const val SUPABASEKT = "0.5.2"
+    const val SUPABASEKT = "0.6-alpha-1"
     const val ANDROID_COMPAT = "1.5.1"
     const val COMPOSE = "1.2.0-beta02"
     const val NAPIER = "2.6.1"
     const val DATETIME = "0.4.0"
     const val STATELY = "1.2.2"
-    const val ATOMICFU = "0.18.3"
+    const val ATOMICFU = "0.18.5"
     const val SETTINGS = "1.0.0-RC"
 
 }
