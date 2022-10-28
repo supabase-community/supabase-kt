00aaac06b5378a967fae9bad1718a39ba934d900
TheRealJan <jan.m.tennert@gmail.com>
2022-10-28 13:16:27 +0200

    Add RealtimeDemo

---
+++ desktopDemo/src/jvmMain/kotlin/RealtimeDemo.kt
@@ -0,0 +1,29 @@
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.GoTrue
+import io.github.jan.supabase.realtime.PostgresAction
+import io.github.jan.supabase.realtime.Realtime
+import io.github.jan.supabase.realtime.createChannel
+import io.github.jan.supabase.realtime.postgresChangeFlow
+import io.github.jan.supabase.realtime.realtime
+
+suspend fun main() {
+    Napier.base(DebugAntilog())
+    val client = createSupabaseClient(
+        supabaseUrl = System.getenv("SUPABASE_URL"),
+        supabaseKey = System.getenv("SUPABASE_KEY")
+    ) {
+        install(GoTrue)
+        install(Realtime)
+    }
+    client.realtime.connect()
+    val channel = client.realtime.createChannel("channelId")
+    val changes = channel.postgresChangeFlow<PostgresAction.Update>("public") {
+        table = "test"
+    }
+    channel.join()
+    changes.collect {
+        println(it)
+    }
+}
