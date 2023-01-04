b4bd01a033b027b98fb40e8c7fdd29e7945c47b7
TheRealJan <jan.m.tennert@gmail.com>
2023-01-04 04:14:19 +0100

    Fix Publishing

--- buildSrc/src/main/kotlin/Publishing.kt
+++ buildSrc/src/main/kotlin/Publishing.kt
@@ -1,6 +1,6 @@
 object Publishing {
 
-    val REPOSITORY_ID = "iogithubjan-tennert-1193"
+    val REPOSITORY_ID = System.getenv("SONATYPE_REPOSITORY_ID")
     val PROFILE_ID = System.getenv("SONATYPE_PROFILE_ID")
     val SONATYPE_USERNAME = System.getenv("OSSRH_USERNAME")
     val SONATYPE_PASSWORD = System.getenv("OSSRH_PASSWORD")
