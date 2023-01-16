d7dbac01383421543e7785cd6528cd16fe710add
TheRealJan <jan.m.tennert@gmail.com>
2023-01-16 15:59:54 +0100

    Make BucketItem properties nullable in case the item is a folder (#45)

--- Storage/src/commonMain/kotlin/io/github/jan/supabase/storage/BucketItem.kt
+++ Storage/src/commonMain/kotlin/io/github/jan/supabase/storage/BucketItem.kt
@@ -5,15 +5,18 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonObject
 
+/**
+ * Represents a file or a folder in a bucket. If the item is a folder, everything except [name] is null.
+ */
 @Serializable
 data class BucketItem(
     val name: String,
-    val id: String,
+    val id: String?,
     @SerialName("updated_at")
-    val updatedAt: Instant,
+    val updatedAt: Instant?,
     @SerialName("created_at")
-    val createdAt: Instant,
+    val createdAt: Instant?,
     @SerialName("last_accessed_at")
-    val lastAccessedAt: Instant,
-    val metadata: JsonObject
+    val lastAccessedAt: Instant?,
+    val metadata: JsonObject?
 )
