a57068095a04c62c956ff0571bfb41145e5e6f57
TheRealJan <jan.m.tennert@gmail.com>
2023-02-02 21:12:20 +0100

    Update demo

--- demo/buildSrc/src/main/kotlin/Versions.kt
+++ demo/buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
 
     const val KOIN = "3.3.2"
-    const val SUPABASE = "0.8.0-alpha-1"
+    const val SUPABASE = "0.8.0-beta-1"
     const val LIFECYCLE = "2.5.1"
     const val KTOR = "2.2.2"
 
