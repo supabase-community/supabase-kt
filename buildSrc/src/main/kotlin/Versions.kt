2eeb01a044ff3dc8525ebe6e9a25beced21adfbc
TheRealJan <jan.m.tennert@gmail.com>
2023-02-08 17:57:48 +0100

    Update Kotlin and Ktor

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,14 +1,14 @@
 object Versions {
 
-    const val KOTLIN = "1.8.0"
-    const val KTOR = "2.2.2"
+    const val KOTLIN = "1.8.10"
+    const val KTOR = "2.2.3"
     const val ANDROID_CORE = "1.9.0"
     const val COROUTINES = "1.6.4"
     const val DOKKA = "1.7.20"
     const val NEXUS_STAGING = "0.30.0"
-    const val SUPABASEKT = "0.8.0-beta-1"
+    const val SUPABASEKT = "0.8.0-beta-2"
     const val ANDROID_COMPAT = "1.5.1"
-    const val COMPOSE = "1.3.0-rc01"
+    const val COMPOSE = "1.3.0"
     const val COMPOSE_COMPILER = "1.3.2.1"
     const val NAPIER = "2.6.1"
     const val DATETIME = "0.4.0"
