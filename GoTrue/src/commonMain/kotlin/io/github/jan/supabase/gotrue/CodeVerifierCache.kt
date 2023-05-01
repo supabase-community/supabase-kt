5763ec6568d17b4f3248c89659279f28cc0df086
TheRealJan <jan.m.tennert@gmail.com>
2023-05-01 19:15:54 +0200

    Improve documentation
    Signed-off-by: TheRealJan <jan.m.tennert@gmail.com>

--- GoTrue/src/commonMain/kotlin/io/github/jan/supabase/gotrue/CodeVerifierCache.kt
+++ GoTrue/src/commonMain/kotlin/io/github/jan/supabase/gotrue/CodeVerifierCache.kt
@@ -4,16 +4,31 @@ import com.russhwolf.settings.ExperimentalSettingsApi
 import com.russhwolf.settings.Settings
 import com.russhwolf.settings.coroutines.toSuspendSettings
 
+/**
+ * A cache for the code verifier used in the PKCE flow.
+ */
 interface CodeVerifierCache {
 
+    /**
+     * Saves the given code verifier.
+     */
     suspend fun saveCodeVerifier(codeVerifier: String)
 
+    /**
+     * Loads the saved code verifier from the cache.
+     */
     suspend fun loadCodeVerifier(): String?
 
+    /**
+     * Deletes the saved code verifier from the cache.
+     */
     suspend fun deleteCodeVerifier()
 
 }
 
+/**
+ * A [CodeVerifierCache] that uses the [Settings] API.
+ */
 class SettingsCodeVerifierCache(settings: Settings = createDefaultSettings()): CodeVerifierCache {
 
     @OptIn(ExperimentalSettingsApi::class)
