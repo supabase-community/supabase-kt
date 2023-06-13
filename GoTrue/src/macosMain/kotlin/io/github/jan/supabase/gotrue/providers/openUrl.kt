7b3197248084b6262e0f3a202d8f793203101172
TheRealJan <jan.m.tennert@gmail.com>
2023-06-13 22:14:04 +0200

    Add error checking on macos
    Signed-off-by: TheRealJan <jan.m.tennert@gmail.com>

--- GoTrue/src/macosMain/kotlin/io/github/jan/supabase/gotrue/providers/openUrl.kt
+++ GoTrue/src/macosMain/kotlin/io/github/jan/supabase/gotrue/providers/openUrl.kt
@@ -1,8 +1,12 @@
 package io.github.jan.supabase.gotrue.providers
 
+import co.touchlab.kermit.Logger
 import platform.AppKit.NSWorkspace
+import platform.AppKit.NSWorkspaceOpenConfiguration
 import platform.Foundation.NSURL
 
 internal actual fun openUrl(url: NSURL) {
-    NSWorkspace.sharedWorkspace().openURL(url)
+    NSWorkspace.sharedWorkspace().openURL(url, NSWorkspaceOpenConfiguration()) { _, error ->
+        if(error != null) Logger.d { "Successfully opened provider url in safari" } else Logger.e { "Failed to open provider url in safari" }
+    }
 }
