764aad8f115483ac271b1f3d46f49ac4e3baa791
Jan Tennert <jan.m.tennert@gmail.com>
2023-06-14 17:21:59 +0200

    Add X-Client-Info header (#197)
    * Add client info header to requests
    
    Signed-off-by: TheRealJan <jan.m.tennert@gmail.com>
    
    * Fix invalid directory name
    
    Signed-off-by: TheRealJan <jan.m.tennert@gmail.com>
    
    * Add internal annotation to build config
    
    Signed-off-by: TheRealJan <jan.m.tennert@gmail.com>
    
    * add tests
    
    Signed-off-by: TheRealJan <jan.m.tennert@gmail.com>
    
    ---------
    
    Signed-off-by: TheRealJan <jan.m.tennert@gmail.com>

--- src/commonMain/kotlin/io/github/jan/supabase/network/KtorSupabaseHttpClient.kt
+++ src/commonMain/kotlin/io/github/jan/supabase/network/KtorSupabaseHttpClient.kt
@@ -2,6 +2,7 @@
 package io.github.jan.supabase.network
 
 import co.touchlab.kermit.Logger
+import io.github.jan.supabase.BuildConfig
 import io.github.jan.supabase.annotiations.SupabaseInternal
 import io.github.jan.supabase.exceptions.HttpRequestException
 import io.github.jan.supabase.supabaseJson
@@ -86,6 +87,7 @@ class KtorSupabaseHttpClient @SupabaseInternal constructor(
                 if(supabaseKey.isNotBlank()) {
                     append("apikey", supabaseKey)
                 }
+                append("X-Client-Info", "supabase-kt/${BuildConfig.PROJECT_VERSION}")
             }
             port = HTTPS_PORT
         }
