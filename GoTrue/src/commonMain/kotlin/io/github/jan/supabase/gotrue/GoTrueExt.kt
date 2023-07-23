c83a0bee2717cb753ec28cc91dde7dde3bf16beb
TheRealJan <jan.m.tennert@gmail.com>
2023-07-23 15:43:51 +0200

    Add GoTrueExtensions
    Signed-off-by: TheRealJan <jan.m.tennert@gmail.com>

---
+++ GoTrue/src/commonMain/kotlin/io/github/jan/supabase/gotrue/GoTrueExt.kt
@@ -0,0 +1,43 @@
+package io.github.jan.supabase.gotrue
+
+import co.touchlab.kermit.Logger
+import io.github.jan.supabase.gotrue.user.UserSession
+
+/**
+ * Parses a session from a fragment.
+ * @param fragment The fragment to parse the session from
+ * @return The parsed session. Note that the user will be null, but you can retrieve it using [GoTrue.retrieveUser]
+ */
+fun GoTrue.parseSessionFromFragment(fragment: String): UserSession {
+    val sessionParts = fragment.split("&").associate {
+        it.split("=").let { pair ->
+            pair[0] to pair[1]
+        }
+    }
+    Logger.d { "Fragment parts: $sessionParts" }
+
+    val accessToken = sessionParts["access_token"] ?: invalidArg("No access token found")
+    val refreshToken = sessionParts["refresh_token"] ?: invalidArg("No refresh token found")
+    val expiresIn = sessionParts["expires_in"]?.toLong() ?: invalidArg("No expires in found")
+    val tokenType = sessionParts["token_type"] ?: invalidArg("No token type found")
+    val type = sessionParts["type"] ?: ""
+    val providerToken = sessionParts["provider_token"]
+    val providerRefreshToken = sessionParts["provider_refresh_token"]
+    return UserSession(
+        accessToken = accessToken,
+        refreshToken = refreshToken,
+        providerRefreshToken = providerRefreshToken,
+        providerToken = providerToken,
+        expiresIn = expiresIn,
+        tokenType = tokenType,
+        user = null,
+        type = type
+    )
+}
+
+/**
+ * Parses a session from an url.
+ * @param url The url to parse the session from
+ * @return The parsed session. Note that the user will be null, but you can retrieve it using [GoTrue.retrieveUser]
+ */
+fun GoTrue.parseSessionFromUrl(url: String): UserSession = parseSessionFromFragment(url.substringAfter("#"))
