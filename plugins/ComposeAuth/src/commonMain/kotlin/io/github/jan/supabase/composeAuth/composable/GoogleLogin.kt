27b7030be2ed1caf52ab7e6e98a11ca7f837dea8
temk0 <temko.dev@gmail.com>
2023-07-27 16:43:20 +0200

    renamed GoogleButton, made loginWithGoogle internal and added nonce as parameter

--- plugins/ComposeAuth/src/commonMain/kotlin/io/github/jan/supabase/composeAuth/composable/GoogleLogin.kt
+++
@@ -1,7 +0,0 @@
-package io.github.jan.supabase.composeAuth.composable
-
-import androidx.compose.runtime.Composable
-import io.github.jan.supabase.composeAuth.ComposeAuth
-
-@Composable
-expect fun ComposeAuth.rememberLoginWithGoogle(onResult: (NativeSignInResult) -> Unit = {}, fallback: () -> Unit = {}): NativeSignInState
