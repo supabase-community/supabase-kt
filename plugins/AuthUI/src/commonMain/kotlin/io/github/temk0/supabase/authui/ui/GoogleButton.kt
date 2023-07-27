e475bde61e590aba14635f9bdf2be0756bb27b38
temk0 <temko.dev@gmail.com>
2023-07-27 15:39:10 +0200

    added default values

--- plugins/AuthUI/src/commonMain/kotlin/io/github/temk0/supabase/authui/ui/GoogleButton.kt
+++ plugins/AuthUI/src/commonMain/kotlin/io/github/temk0/supabase/authui/ui/GoogleButton.kt
@@ -4,4 +4,4 @@ import androidx.compose.runtime.Composable
 import io.github.temk0.supabase.authui.AuthUI
 
 @Composable
-expect fun AuthUI.rememberLoginWithGoogle(onResult: (NativeSignInResult) -> Unit, fallback: (() -> Unit)): NativeSignInState
+expect fun AuthUI.rememberLoginWithGoogle(onResult: (NativeSignInResult) -> Unit = {} , fallback: () -> Unit = {/*use gotrue.loginWith(Google) as default*/}): NativeSignInState
