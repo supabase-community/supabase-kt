7e99eeeff93f5b2f6c1392f66b205a2e45142471
Hieu Vu <star.hieu.99@gmail.com>
2023-09-14 16:10:08 +0700

    Update test

--- Postgrest/src/commonTest/kotlin/io.github.jan.supabase.postgrest/executor/RestRequestExecutorTest.kt
+++ Postgrest/src/commonTest/kotlin/io.github.jan.supabase.postgrest/executor/RestRequestExecutorTest.kt
@@ -1,5 +1,15 @@
 package io.github.jan.supabase.postgrest.executor
 
+import io.github.jan.supabase.postgrest.PostgrestImpl
+import io.github.jan.supabase.postgrest.query.Returning
+import io.github.jan.supabase.postgrest.request.DeleteRequest
+import io.mockative.Mock
+import io.mockative.classOf
+import io.mockative.every
+import io.mockative.mock
+import io.mockative.once
+import io.mockative.verify
+import kotlinx.coroutines.test.runTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertTrue
@@ -8,18 +18,30 @@ class RestRequestExecutorTest {
 
     lateinit var testee: RestRequestExecutor
 
+    @Mock
+    private val postgrest = mock(classOf<PostgrestImpl>())
 
-    // Need a mock mechanism to test
-    class Car() {
-
-    }
     @BeforeTest
     fun setUp() {
+        testee = RestRequestExecutor(postgrest = postgrest)
     }
 
     @Test
-    fun testExecuteDeleteRequest_returnSuccess() {
-        assertTrue(true)
+    fun testExecuteDeleteRequest_returnSuccess() = runTest {
+        val request = DeleteRequest(
+            returning = Returning.REPRESENTATION,
+            count = null,
+            filter = mapOf(),
+            schema = ""
+        )
+
+        every { postgrest.apiVersion }
+            .returns(4)
+        testee.execute("/user", request)
+
+        verify {
+            postgrest.api
+        }.wasInvoked(exactly = once)
     }
 
     @Test
