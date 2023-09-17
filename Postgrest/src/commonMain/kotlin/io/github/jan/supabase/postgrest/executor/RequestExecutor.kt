a0c3480de1e4ce40b7a0b28b51ec6239d2fed3a0
Hieu Vu <star.hieu.99@gmail.com>
2023-09-17 23:33:48 +0700

    Add document

--- Postgrest/src/commonMain/kotlin/io/github/jan/supabase/postgrest/executor/RequestExecutor.kt
+++ Postgrest/src/commonMain/kotlin/io/github/jan/supabase/postgrest/executor/RequestExecutor.kt
@@ -1,15 +1,22 @@
 package io.github.jan.supabase.postgrest.executor
 
-import io.github.jan.supabase.bodyOrNull
-import io.github.jan.supabase.postgrest.Postgrest
-import io.github.jan.supabase.postgrest.result.PostgrestResult
 import io.github.jan.supabase.postgrest.request.PostgrestRequest
-import io.ktor.client.statement.HttpResponse
+import io.github.jan.supabase.postgrest.result.PostgrestResult
 
+/**
+ * Request executor
+ *
+ * @constructor Create empty Request executor
+ */
 interface RequestExecutor {
 
+    /**
+     * Execute given PostgrestRequest
+     *
+     * @param path [String]
+     * @param request [PostgrestRequest]
+     * @return [PostgrestResult]
+     */
     suspend fun execute(path: String, request: PostgrestRequest): PostgrestResult
-    suspend fun HttpResponse.asPostgrestResult(postgrest: Postgrest): PostgrestResult =
-        PostgrestResult(bodyOrNull(), headers, postgrest)
 
 }
--- Postgrest/src/commonMain/kotlin/io/github/jan/supabase/postgrest/executor/RestRequestExecutor.kt
+++ Postgrest/src/commonMain/kotlin/io/github/jan/supabase/postgrest/executor/RestRequestExecutor.kt
@@ -1,47 +1,22 @@
 package io.github.jan.supabase.postgrest.executor
 
-import io.github.jan.supabase.gotrue.GoTrue
 import io.github.jan.supabase.postgrest.Postgrest
-import io.github.jan.supabase.postgrest.query.PostgrestBuilder
 import io.github.jan.supabase.postgrest.request.PostgrestRequest
 import io.github.jan.supabase.postgrest.result.PostgrestResult
-import io.ktor.client.request.accept
-import io.ktor.client.request.bearerAuth
-import io.ktor.client.request.header
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpMethod
-import io.ktor.http.contentType
-import io.ktor.http.parametersOf
 
-class RestRequestExecutor constructor(private val postgrest: Postgrest): RequestExecutor {
-   override suspend fun execute(
+/**
+ * Implementation of [RestRequestExecutor], containing logic of making API to Supabase's Postgrest APIs
+ *
+ * @property postgrest: instance of Postgrest
+ * @constructor Create with instance of Postgrest
+ */
+class RestRequestExecutor constructor(private val postgrest: Postgrest) : RequestExecutor {
+    override suspend fun execute(
         path: String,
         request: PostgrestRequest
     ): PostgrestResult {
         return postgrest.api.request(path) {
-            method = request.method
-            contentType(ContentType.Application.Json)
-            val token = postgrest.config.jwtToken
-                ?: postgrest.supabaseClient.pluginManager.getPluginOrNull(GoTrue)?.currentAccessTokenOrNull()
-            token?.let {
-                bearerAuth(it)
-            }
-            if (request.single) {
-                accept(ContentType(HttpHeaders.Accept, "application/vnd.pgrst.object+json"))
-            }
-            headers[PostgrestBuilder.HEADER_PREFER] = request.prefer.joinToString(",")
-            with(url.parameters) {
-                appendAll(parametersOf(request.urlParams.mapValues { (_, value) -> listOf(value) }))
-                appendAll(parametersOf(request.filter.mapValues { (_, value) -> listOf(value.first()) }))
-            }
-            if (request.schema.isNotBlank()) {
-                if (method in listOf(HttpMethod.Get, HttpMethod.Head)) {
-                    header("Accept-Profile", request.schema)
-                } else {
-                    header("Content-Profile", request.schema)
-                }
-            }
+            configurePostgrestRequest(request, postgrest)
         }.asPostgrestResult(postgrest)
     }
 }
