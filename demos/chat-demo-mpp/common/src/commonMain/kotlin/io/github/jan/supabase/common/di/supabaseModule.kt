732703b9cafab86200d47a0859f914a1ab29ab05
Jan <jan.m.tennert@gmail.com>
2023-12-28 14:07:57 +0100

    remove accidental change

--- demos/chat-demo-mpp/common/src/commonMain/kotlin/io/github/jan/supabase/common/di/supabaseModule.kt
+++ demos/chat-demo-mpp/common/src/commonMain/kotlin/io/github/jan/supabase/common/di/supabaseModule.kt
@@ -14,8 +14,8 @@ expect fun AuthConfig.platformGoTrueConfig()
 val supabaseModule = module {
     single {
         createSupabaseClient(
-            supabaseUrl = "https://arnyfaeuskyqfxkvotgj.supabase.co",
-            supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFybnlmYWV1c2t5cWZ4a3ZvdGdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTMwMzkxMTEsImV4cCI6MTk2ODYxNTExMX0.ItmL8lfnOL9oy7CEX9N6TnYt10VVhk-KTlwley4aq1M"
+            supabaseUrl = "YOUR_URL",
+            supabaseKey = "YOUR_KEY"
         ) {
             install(Postgrest)
             install(Auth) {
