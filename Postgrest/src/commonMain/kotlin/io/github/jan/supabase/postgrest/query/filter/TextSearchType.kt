4b1cf04862b142560b533b3bfe91ce68a6e61daf
Jan Tennert <jan.m.tennert@gmail.com>
2023-12-30 22:04:55 +0100

    Fix textSearch method making an invalid query (#409)
    * Fix fts
    
    * update version

--- Postgrest/src/commonMain/kotlin/io/github/jan/supabase/postgrest/query/filter/TextSearchType.kt
+++ Postgrest/src/commonMain/kotlin/io/github/jan/supabase/postgrest/query/filter/TextSearchType.kt
@@ -9,8 +9,8 @@ package io.github.jan.supabase.postgrest.query.filter
  * Used to search rows using a full text search. See [Postgrest](https://postgrest.org/en/stable/api.html#full-text-search) for more information
  */
 enum class TextSearchType(val identifier: String) {
-    TSVECTOR("tsvector"),
-    PLAINTO("plainto"),
-    PHRASETO("phraseto"),
-    WEBSEARCH("websearch")
+    NONE(""),
+    PLAINTO("pl"),
+    PHRASETO("ph"),
+    WEBSEARCH("w")
 }
