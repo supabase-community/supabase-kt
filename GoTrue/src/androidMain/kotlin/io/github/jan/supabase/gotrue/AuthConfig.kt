a045a12778c5720c97a65f3bed418c3f742cd848
Jan <jan.m.tennert@gmail.com>
2024-02-14 18:16:01 +0100

    Fix Android implementation

--- GoTrue/src/androidMain/kotlin/io/github/jan/supabase/gotrue/AuthConfig.kt
+++ GoTrue/src/androidMain/kotlin/io/github/jan/supabase/gotrue/AuthConfig.kt
@@ -2,6 +2,24 @@ package io.github.jan.supabase.gotrue
 
 import io.github.jan.supabase.gotrue.ExternalAuthAction.CUSTOM_TABS
 import io.github.jan.supabase.gotrue.ExternalAuthAction.EXTERNAL_BROWSER
+import io.github.jan.supabase.plugins.CustomSerializationConfig
+
+/**
+ * The configuration for [Auth]
+ */
+actual class AuthConfig : CustomSerializationConfig, AuthConfigDefaults() {
+
+    /**
+     * Whether to stop auto-refresh on focus loss, and resume it on focus again
+     */
+    var enableLifecycleCallbacks: Boolean = true
+
+    /**
+     * The action to use for the OAuth flow
+     */
+    var defaultExternalAuthAction: ExternalAuthAction = ExternalAuthAction.EXTERNAL_BROWSER
+
+}
 
 /**
  * Represents the available actions for external auth such as OAuth and SSO.
