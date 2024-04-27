57fd67d7d1cd388d3836e27c1964fedd679cf080
Jan <jan.m.tennert@gmail.com>
2024-04-27 23:58:05 +0200

    add gradle toolchain plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
+}
+
 include("GoTrue")
 include("Postgrest")
 include("Storage")
