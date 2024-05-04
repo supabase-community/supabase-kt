26f76d1b4f833f886bb3aebc5935a09f1d427ecf
Jan <jan.m.tennert@gmail.com>
2024-05-04 15:07:42 +0200

    add channel name parameter to the methods

--- Realtime/src/commonMain/kotlin/io/github/jan/supabase/realtime/PostgrestExtensions.kt
+++ Realtime/src/commonMain/kotlin/io/github/jan/supabase/realtime/PostgrestExtensions.kt
@@ -14,14 +14,16 @@ import kotlin.reflect.KProperty1
  * This function listens for changes in the table and emits the new value whenever a change occurs.
  * @param primaryKey the primary key of the [Data] type
  * @param filter the filter to apply to the select query
+ * @param channelName the name of the channel to use for the realtime updates. If null, a channel name following the format "schema:table:id" will be generated
  */
 inline fun <reified Data : Any> PostgrestQueryBuilder.selectSingleValueAsFlow(
     primaryKey: PrimaryKey<Data>,
+    channelName: String? = null,
     crossinline filter: PostgrestFilterBuilder.() -> Unit
 ): Flow<Data> {
     val realtime = postgrest.supabaseClient.realtime as RealtimeImpl
     val id = realtime.nextIncrementId()
-    val channel = realtime.channel("$schema:$table:$id")
+    val channel = realtime.channel(channelName ?: "$schema:$table:$id")
     return flow {
         val dataFlow = channel.postgresSingleDataFlow(
             schema = this@selectSingleValueAsFlow.schema,
@@ -41,25 +43,29 @@ inline fun <reified Data : Any> PostgrestQueryBuilder.selectSingleValueAsFlow(
  * This function listens for changes in the table and emits the new value whenever a change occurs.
  * @param primaryKey the primary key of the [Data] type
  * @param filter the filter to apply to the select query
+ * @param channelName the name of the channel to use for the realtime updates. If null, a channel name following the format "schema:table:id" will be generated
  */
 inline fun <reified Data : Any, Value> PostgrestQueryBuilder.selectSingleValueAsFlow(
     primaryKey: KProperty1<Data, Value>,
+    channelName: String? = null,
     crossinline filter: PostgrestFilterBuilder.() -> Unit
-): Flow<Data> = selectSingleValueAsFlow(PrimaryKey(primaryKey.name) { primaryKey.get(it).toString() }, filter)
+): Flow<Data> = selectSingleValueAsFlow(PrimaryKey(primaryKey.name) { primaryKey.get(it).toString() }, channelName, filter)
 
 /**
  * Executes vertical filtering with select on [PostgrestQueryBuilder.table] and [PostgrestQueryBuilder.schema] and returns a [Flow] of a list of values matching the [filter].
  * This function listens for changes in the table and emits the new list whenever a change occurs.
  * @param primaryKey the primary key of the [Data] type
  * @param filter the filter to apply to the select query
+ * @param channelName the name of the channel to use for the realtime updates. If null, a channel name following the format "schema:table:id" will be generated
  */
 inline fun <reified Data : Any> PostgrestQueryBuilder.selectAsFlow(
     primaryKey: PrimaryKey<Data>,
+    channelName: String? = null,
     filter: FilterOperation? = null,
 ): Flow<List<Data>> {
     val realtime = postgrest.supabaseClient.realtime as RealtimeImpl
     val id = realtime.nextIncrementId()
-    val channel = realtime.channel("$schema:$table:$id")
+    val channel = realtime.channel(channelName ?: "$schema:$table:$id")
     return flow {
         val dataFlow = channel.postgresListDataFlow(
             schema = this@selectAsFlow.schema,
@@ -79,8 +85,10 @@ inline fun <reified Data : Any> PostgrestQueryBuilder.selectAsFlow(
  * This function listens for changes in the table and emits the new list whenever a change occurs.
  * @param primaryKey the primary key of the [Data] type
  * @param filter the filter to apply to the select query
+ * @param channelName the name of the channel to use for the realtime updates. If null, a channel name following the format "schema:table:id" will be generated
  */
 inline fun <reified Data : Any, Value> PostgrestQueryBuilder.selectAsFlow(
     primaryKey: KProperty1<Data, Value>,
+    channelName: String? = null,
     filter: FilterOperation? = null,
-): Flow<List<Data>> = selectAsFlow(PrimaryKey(primaryKey.name) { primaryKey.get(it).toString() }, filter)
+): Flow<List<Data>> = selectAsFlow(PrimaryKey(primaryKey.name) { primaryKey.get(it).toString() }, channelName, filter)
