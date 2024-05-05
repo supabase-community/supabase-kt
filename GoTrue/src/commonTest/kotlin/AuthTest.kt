98820ad49e7553ead53baf23dd308df74a33aa3c
Jan <jan.m.tennert@gmail.com>
2024-05-05 17:42:23 +0200

    fix tests

--- GoTrue/src/commonTest/kotlin/AuthTest.kt
+++ GoTrue/src/commonTest/kotlin/AuthTest.kt
@@ -4,9 +4,12 @@ import io.github.jan.supabase.gotrue.MemorySessionManager
 import io.github.jan.supabase.gotrue.SessionStatus
 import io.github.jan.supabase.gotrue.auth
 import io.github.jan.supabase.gotrue.minimalSettings
+import io.github.jan.supabase.gotrue.providers.Github
 import io.github.jan.supabase.gotrue.user.UserSession
 import io.github.jan.supabase.testing.createMockedSupabaseClient
+import io.github.jan.supabase.testing.pathAfterVersion
 import io.github.jan.supabase.testing.respondJson
+import io.ktor.http.Url
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -68,7 +71,13 @@ class AuthTest {
     fun testImportExpiredSession() {
         runTest {
             val newSession = userSession()
-            val client = createMockedSupabaseClient(configuration = configuration) {
+            val client = createMockedSupabaseClient(configuration = {
+                install(Auth) {
+                    minimalSettings(
+                        alwaysAutoRefresh = true
+                    )
+                }
+            }) {
                 respondJson(newSession)
             }
             client.auth.awaitInitialization()
@@ -104,6 +113,44 @@ class AuthTest {
             client.auth.startAutoRefreshForCurrentSession()
             assertEquals(newSession.expiresIn, client.auth.currentSessionOrNull()?.expiresIn)
         }
+    }
+
+    @Test
+    fun testGetOAuthUrl() {
+        runTest {
+            val expectedProvider = Github
+            val expectedRedirectUrl = "https://example.com/auth/callback"
+            val endpoint = "authorize/custom/endpoint"
+            val supabaseUrl = "https://id.supabase.co"
+            val client = createMockedSupabaseClient(supabaseUrl = supabaseUrl, configuration = configuration)
+            client.auth.awaitInitialization()
+            val url = Url(client.auth.getOAuthUrl(expectedProvider, expectedRedirectUrl, endpoint) {
+                queryParams["key"] = "value"
+                scopes.add("scope1")
+                scopes.add("scope2")
+            })
+            assertEquals(
+                endpoint,
+                url.pathAfterVersion().substring(1)
+            )
+            assertEquals(
+                expectedProvider.name,
+                url.parameters["provider"]
+            )
+            assertEquals(
+                expectedRedirectUrl,
+                url.parameters["redirect_to"]
+            )
+            assertEquals(
+                "value",
+                url.parameters["key"]
+            )
+            assertEquals(
+                "scope1 scope2",
+                url.parameters["scopes"]
+            )
+        }
+
     }
 
 }
