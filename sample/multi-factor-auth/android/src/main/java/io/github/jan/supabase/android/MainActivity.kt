ee96736e6b5a9c17c9b14713513ec875057d285f
Jan <jan.m.tennert@gmail.com>
2024-07-26 20:39:30 +0200

    Fix Android build

--- sample/multi-factor-auth/android/src/main/java/io/github/jan/supabase/android/MainActivity.kt
+++ sample/multi-factor-auth/android/src/main/java/io/github/jan/supabase/android/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
-
-import co.touchlab.kermit.Logger
 import io.github.jan.supabase.common.App
 import io.github.jan.supabase.common.AppViewModel
 import io.github.jan.supabase.gotrue.handleDeeplinks
@@ -17,7 +15,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Logger.base(DebugAntilog())
         viewModel.supabaseClient.handleDeeplinks(intent)
         setContent {
             MaterialTheme {
