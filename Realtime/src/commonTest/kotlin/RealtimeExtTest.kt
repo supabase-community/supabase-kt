0a70bc5a0320e392cc5c49f99424a371c2a68fd5
Jan <jan.m.tennert@gmail.com>
2024-07-30 14:42:45 +0200

    restructure extension tests

--- Realtime/src/commonTest/kotlin/RealtimeExtTest.kt
+++ Realtime/src/commonTest/kotlin/RealtimeExtTest.kt
@@ -13,9 +13,6 @@ import io.github.jan.supabase.realtime.presenceDataFlow
 import io.github.jan.supabase.serializer.KotlinXSerializer
 import io.github.jan.supabase.testing.pathAfterVersion
 import io.ktor.client.engine.mock.respond
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import kotlinx.datetime.Clock
 import kotlinx.serialization.Serializable
@@ -51,22 +48,16 @@ class RealtimeExtTest {
                 },
                 supabaseHandler = {
                     val channel = it.channel("channelId")
-                    coroutineScope {
-                        launch {
-                            val broadcastFlow = channel.presenceDataFlow<DummyData>()
-                            broadcastFlow.test(FLOW_TIMEOUT) {
-                                assertContentEquals(firstPresenceList, awaitItem())
-                                assertContentEquals(secondPresenceList, awaitItem())
-                                assertContentEquals(emptyList(), awaitItem())
-                            }
-                        }
-                        launch {
-                            channel.subscribe(true)
-                            channel.callbackManager.triggerPresence(firstPresenceList, emptyList())
-                            channel.callbackManager.triggerPresence(listOf(DummyData(3)), listOf(DummyData(2)))
-                            channel.callbackManager.triggerPresence(emptyList(), secondPresenceList)
-                        }
-                    }.join()
+                    val broadcastFlow = channel.presenceDataFlow<DummyData>()
+                    broadcastFlow.test(FLOW_TIMEOUT) {
+                        channel.subscribe(true)
+                        channel.callbackManager.triggerPresence(firstPresenceList, emptyList())
+                        assertContentEquals(firstPresenceList, awaitItem())
+                        channel.callbackManager.triggerPresence(listOf(DummyData(3)), listOf(DummyData(2)))
+                        assertContentEquals(secondPresenceList, awaitItem())
+                        channel.callbackManager.triggerPresence(emptyList(), secondPresenceList)
+                        assertContentEquals(emptyList(), awaitItem())
+                    }
                 }
             )
         }
@@ -88,27 +79,19 @@ class RealtimeExtTest {
                         filter = filter,
                         primaryKey = DummyData::key
                     )
-                    dataFlow.first() //The initial data has to be waited for first because otherwise the callbacks are not registered yet
-                    //Now we can test updates
-                    coroutineScope {
-                        launch {
-                            dataFlow.test(FLOW_TIMEOUT) {
-                                assertContentEquals(listOf(DummyData(0, "first")), awaitItem()) //1.
-                                assertContentEquals(listOf(DummyData(0, "second")), awaitItem()) //2.
-                                assertContentEquals(listOf(DummyData(0, "second"), DummyData(1, "third")), awaitItem())//3.
-                                assertContentEquals(listOf(DummyData(0, "fourth"), DummyData(1, "third")), awaitItem())//4.
-                                assertContentEquals(listOf(DummyData(0, "fourth")), awaitItem()) //5.
-                            }
-                        }
-                        launch {
-                            channel.subscribe(true)
-                            channel.callbackManager.setServerChanges(listOf(PostgresJoinConfig("public", "table", "id=eq.0", "*", 0)))
-                            channel.callbackManager.triggerPostgresChange<PostgresAction.Update>(DummyData(0, "second"), DummyData(0, "first")) //2.
-                            channel.callbackManager.triggerPostgresChange<PostgresAction.Insert>(DummyData(1, "third"), null) //3.
-                            channel.callbackManager.triggerPostgresChange<PostgresAction.Update>(DummyData(0, "fourth"), DummyData(0, "second")) //4.
-                            channel.callbackManager.triggerPostgresChange<PostgresAction.Delete>(null, DummyData(1)) //5.
-                        }
-                    }.join()
+                    dataFlow.test(FLOW_TIMEOUT) {
+                        assertContentEquals(listOf(DummyData(0, "first")), awaitItem()) //1. Initial data
+                        channel.subscribe(true)
+                        channel.callbackManager.setServerChanges(listOf(PostgresJoinConfig("public", "table", "id=eq.0", "*", 0)))
+                        channel.callbackManager.triggerPostgresChange<PostgresAction.Update>(DummyData(0, "second"), DummyData(0, "first")) //2.
+                        assertContentEquals(listOf(DummyData(0, "second")), awaitItem()) //2.
+                        channel.callbackManager.triggerPostgresChange<PostgresAction.Insert>(DummyData(1, "third"), null) //3.
+                        assertContentEquals(listOf(DummyData(0, "second"), DummyData(1, "third")), awaitItem())//3.
+                        channel.callbackManager.triggerPostgresChange<PostgresAction.Update>(DummyData(0, "fourth"), DummyData(0, "second")) //4.
+                        assertContentEquals(listOf(DummyData(0, "fourth"), DummyData(1, "third")), awaitItem())//4.
+                        channel.callbackManager.triggerPostgresChange<PostgresAction.Delete>(null, DummyData(1)) //5.
+                        assertContentEquals(listOf(DummyData(0, "fourth")), awaitItem()) //5.
+                    }
                 },
                 mockEngineHandler = {
                     assertEquals("/table", it.url.pathAfterVersion())
@@ -135,24 +118,15 @@ class RealtimeExtTest {
                     val dataFlow = channel.postgresSingleDataFlow("public", "table", primaryKey = DummyData::key) {
                         eq("id", 0)
                     }
-                    dataFlow.first() //The initial data has to be waited for first because otherwise the callbacks are not registered yet
-
-                    //Now we can test updates
-                    coroutineScope {
-                        launch {
-                            dataFlow.test(FLOW_TIMEOUT) {
-                                assertEquals(DummyData(0, "content"), awaitItem()) //1.
-                                assertEquals(DummyData(0, "content4"), awaitItem()) //2.
-                                awaitComplete() //The flow will complete after the deletion
-                            }
-                        }
-                        launch {
-                            channel.subscribe(true)
-                            channel.callbackManager.setServerChanges(listOf(PostgresJoinConfig("public", "table", "key=eq.0", "*", 0))) //The method will add a filter for the primary key. In this case 0.
-                            channel.callbackManager.triggerPostgresChange<PostgresAction.Update>(DummyData(0, "content4"), DummyData(0, "content")) //2.
-                            channel.callbackManager.triggerPostgresChange<PostgresAction.Delete>(null, DummyData(0)) //3.
-                        }
-                    }.join()
+                    dataFlow.test(FLOW_TIMEOUT) {
+                        assertEquals(DummyData(0, "content"), awaitItem()) //1.
+                        channel.subscribe(true)
+                        channel.callbackManager.setServerChanges(listOf(PostgresJoinConfig("public", "table", "key=eq.0", "*", 0))) //The method will add a filter for the primary key. In this case 0.
+                        channel.callbackManager.triggerPostgresChange<PostgresAction.Update>(DummyData(0, "content4"), DummyData(0, "content")) //2.
+                        assertEquals(DummyData(0, "content4"), awaitItem()) //2.
+                        channel.callbackManager.triggerPostgresChange<PostgresAction.Delete>(null, DummyData(0)) //3.
+                        awaitComplete() //The flow will complete after the deletion
+                    }
                 },
                 mockEngineHandler = {
                     assertEquals("/table", it.url.pathAfterVersion())
