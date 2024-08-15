81faa9a52ac995ab63bda78d04d244c16d58ad80
Jan <jan.m.tennert@gmail.com>
2024-08-15 12:04:01 +0200

    Improve docs

--- Supabase/src/commonMain/kotlin/io/github/jan/supabase/AccessTokenProvider.kt
+++ Supabase/src/commonMain/kotlin/io/github/jan/supabase/AccessTokenProvider.kt
@@ -6,9 +6,5 @@ package io.github.jan.supabase
  * obtaining it from the third-party auth client library. Note that this
  * function may be called concurrently and many times. Use memoization and
  * locking techniques if this is not supported by the client libraries.
- *
- * When set, the Auth plugin from `auth-kt` cannot be used.
- * Create another client if you wish to use Supabase Auth and third-party
- * authentications concurrently in the same application.
  */
 typealias AccessTokenProvider = suspend () -> String
