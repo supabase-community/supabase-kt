d2e2b1dfb451b95c27682d5e580de263ba08eb87
Jan <jan.m.tennert@gmail.com>
2024-08-20 16:39:53 +0200

    Add interceptor tests

--- plugins/ApolloGraphQL/src/commonTest/kotlin/ApolloHttpInterceptorTest.kt
+++ plugins/ApolloGraphQL/src/commonTest/kotlin/ApolloHttpInterceptorTest.kt
@@ -1,38 +1,89 @@
-import com.apollographql.mockserver.MockRequestBase
-import com.apollographql.mockserver.MockResponse
-import com.apollographql.mockserver.MockServer
-import com.apollographql.mockserver.MockServerHandler
+import com.apollographql.apollo.annotations.ApolloExperimental
+import com.apollographql.apollo.api.http.HttpMethod
+import com.apollographql.apollo.api.http.HttpRequest
+import com.apollographql.apollo.api.http.HttpResponse
+import com.apollographql.apollo.api.http.get
+import com.apollographql.apollo.network.http.HttpInterceptorChain
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.gotrue.Auth
+import io.github.jan.supabase.gotrue.auth
+import io.github.jan.supabase.graphql.ApolloHttpInterceptor
 import io.github.jan.supabase.graphql.GraphQL
+import io.github.jan.supabase.graphql.graphql
 import io.github.jan.supabase.testing.createMockedSupabaseClient
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApolloHttpInterceptorTest {
 
     @Test
-    fun testApolloHttpInterceptor() {
-        val mockServerHandler = object : MockServerHandler {
-            override fun handle(request: MockRequestBase): MockResponse {
-                println(request.headers)
-                return MockResponse.Builder().build()
-            }
-        }
-        val mockServer = MockServer.Builder().handler(mockServerHandler).build()
+    fun testApolloHttpInterceptorWithApiKey() {
         runTest {
-            val url = mockServer.url()
             val supabaseClient = createMockedSupabaseClient(
-                supabaseUrl = "https://test.supabase.co",
                 supabaseKey = "testkey",
                 configuration = {
-                    install(GraphQL) {
-                        apolloConfiguration {
-                            serverUrl(url)
-                        }
+                    install(GraphQL)
+                }
+            )
+            testInterceptor(supabaseClient, "testkey")
+        }
+    }
+
+    @Test
+    fun testApolloHttpInterceptorWithAuthToken() {
+        runTest {
+            val supabaseClient = createMockedSupabaseClient(
+                configuration = {
+                    install(GraphQL)
+                    install(Auth)
+                }
+            )
+            supabaseClient.auth.importAuthToken("testtoken")
+            testInterceptor(supabaseClient, "testtoken")
+        }
+    }
+
+    @Test
+    fun testApolloHttpInterceptorWithAccessToken() {
+        runTest {
+            val supabaseClient = createMockedSupabaseClient(
+                configuration = {
+                    install(GraphQL)
+                    accessToken = {
+                        "testtoken"
                     }
                 }
             )
-           // supabaseClient.graphql.apolloClient.query(MockQuery("Test")).execute()
+            testInterceptor(supabaseClient, "testtoken")
         }
+    }
+
+    @Test
+    fun testApolloHttpInterceptorWithACustomToken() {
+        runTest {
+            val supabaseClient = createMockedSupabaseClient(
+                configuration = {
+                    install(GraphQL) {
+                        jwtToken = "testtoken"
+                    }
+                }
+            )
+            testInterceptor(supabaseClient, "testtoken")
+        }
+    }
+
+    @OptIn(ApolloExperimental::class)
+    private suspend fun testInterceptor(supabaseClient: SupabaseClient, token: String) {
+        val interceptor = ApolloHttpInterceptor(supabaseClient, supabaseClient.graphql.config)
+        interceptor.intercept(HttpRequest.Builder(HttpMethod.Get, "").build(), object : HttpInterceptorChain {
+
+            override suspend fun proceed(request: HttpRequest): HttpResponse {
+                assertEquals("Bearer $token", request.headers.get("Authorization"))
+                return HttpResponse.Builder(200).build()
+            }
+
+        })
     }
 
 }
--- plugins/ApolloGraphQL/src/commonTest/kotlin/GraphQLTest.kt
+++ plugins/ApolloGraphQL/src/commonTest/kotlin/GraphQLTest.kt
@@ -1,4 +1,5 @@
 import com.apollographql.apollo.api.http.HttpHeader
+import io.github.jan.supabase.BuildConfig
 import io.github.jan.supabase.graphql.ApolloHttpInterceptor
 import io.github.jan.supabase.graphql.GraphQL
 import io.github.jan.supabase.graphql.graphql
@@ -28,6 +29,7 @@ class GraphQLTest {
         assertEquals("https://test.supabase.co/graphql/v1", client.newBuilder().httpServerUrl)
         assertNotNull(client.httpHeaders)
         assertContains(client.httpHeaders!!, HttpHeader("apikey", "testkey"))
+        assertContains(client.httpHeaders!!, HttpHeader("X-Client-Info", "supabase-kt/${BuildConfig.PROJECT_VERSION}"))
         assertTrue { client.newBuilder().httpInterceptors.any { it is ApolloHttpInterceptor } }
         assertEquals(1000, client.newBuilder().webSocketIdleTimeoutMillis)
     }
