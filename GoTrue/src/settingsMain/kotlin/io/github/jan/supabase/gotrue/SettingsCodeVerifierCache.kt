d7d826bb0196a7ef64ccc24099d992e6331e3323
Jan Tennert <jan.m.tennert@gmail.com>
2024-09-09 17:57:38 +0000

    Update Gradle to 10.1 and fix some warnings

--- GoTrue/src/settingsMain/kotlin/io/github/jan/supabase/gotrue/SettingsCodeVerifierCache.kt
+++ GoTrue/src/settingsMain/kotlin/io/github/jan/supabase/gotrue/SettingsCodeVerifierCache.kt
@@ -10,6 +10,7 @@ import com.russhwolf.settings.coroutines.toSuspendSettings
  * @param settings The [Settings] instance to use. Defaults to [createDefaultSettings].
  * @param key The key to use for saving the code verifier.
  */
+@OptIn(ExperimentalSettingsApi::class)
 class SettingsCodeVerifierCache(
     private val settings: Settings = createDefaultSettings(),
     private val key: String = SETTINGS_KEY,
@@ -31,20 +32,16 @@ class SettingsCodeVerifierCache(
         }
     }
 
-    @OptIn(ExperimentalSettingsApi::class)
     private val suspendSettings = settings.toSuspendSettings()
 
-    @OptIn(ExperimentalSettingsApi::class, ExperimentalSettingsApi::class)
     override suspend fun saveCodeVerifier(codeVerifier: String) {
         suspendSettings.putString(key, codeVerifier)
     }
 
-    @OptIn(ExperimentalSettingsApi::class)
     override suspend fun loadCodeVerifier(): String? {
         return suspendSettings.getStringOrNull(key)
     }
 
-    @OptIn(ExperimentalSettingsApi::class)
     override suspend fun deleteCodeVerifier() {
         suspendSettings.remove(key)
     }
