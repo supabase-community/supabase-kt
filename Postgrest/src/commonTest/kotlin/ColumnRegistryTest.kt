b5079f87f937347f7007c5fee3d0e9fae7aaff7e
Jan Tennert <jan.m.tennert@gmail.com>
2024-10-31 15:54:04 +0100

    Fix invalid test type

--- Postgrest/src/commonTest/kotlin/ColumnRegistryTest.kt
+++ Postgrest/src/commonTest/kotlin/ColumnRegistryTest.kt
@@ -7,8 +7,8 @@ class ColumnRegistryTest {
     @Test
     fun testColumnRegistry() {
         val registry = MapColumnRegistry()
-        registry.registerColumns("Test", "column1, column2")
-        assertEquals("column1, column2", registry.getColumns(Test::class))
+        registry.registerColumns("TestType", "column1, column2")
+        assertEquals("column1, column2", registry.getColumns(TestType::class))
     }
 
 }
