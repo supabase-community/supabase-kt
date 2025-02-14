5db0a4e59bb60423191a16d2ee6728fddd337f07
Jan Tennert <jan.m.tennert@gmail.com>
2025-02-14 21:20:23 +0100

    Migrate to dokka v2

--- buildSrc/src/main/kotlin/Dokka.kt
+++ buildSrc/src/main/kotlin/Dokka.kt
@@ -1,30 +1,32 @@
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.withType
-import java.net.URL
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.dokka.gradle.DokkaExtension
+import java.net.URI
 
 fun Project.applyDokkaWithConfiguration() {
-    tasks.withType<org.jetbrains.dokka.gradle.DokkaTaskPartial>().configureEach {
+    extensions.configure(DokkaExtension::class) {
+        val name = when(moduleName.get()) {
+            "supabase-kt" -> "Supabase"
+            "functions-kt" -> "Functions"
+            "auth-kt" -> "Auth"
+            "postgrest-kt" -> "Postgrest"
+            "realtime-kt" -> "Realtime"
+            "storage-kt" -> "Storage"
+            "apollo-graphql" -> "plugins/ApolloGraphQL"
+            "compose-auth" -> "plugins/ComposeAuth"
+            "compose-auth-ui" -> "plugins/ComposeAuthUI"
+            "coil-integration" -> "plugins/CoilIntegration"
+            "coil3-integration" -> "plugins/Coil3Integration"
+            "imageloader-integration" -> "plugins/ImageLoaderIntegration"
+            "serializer-moshi" -> "serializers/Moshi"
+            "serializer-jackson" -> "serializers/Jackson"
+            else -> ""
+        }
         dokkaSourceSets.configureEach {
+      //      includes.from("README.md")
             sourceLink {
-                val name = when(moduleName.get()) {
-                    "supabase-kt" -> "Supabase"
-                    "functions-kt" -> "Functions"
-                    "auth-kt" -> "Auth"
-                    "postgrest-kt" -> "Postgrest"
-                    "realtime-kt" -> "Realtime"
-                    "storage-kt" -> "Storage"
-                    "apollo-graphql" -> "plugins/ApolloGraphQL"
-                    "compose-auth" -> "plugins/ComposeAuth"
-                    "compose-auth-ui" -> "plugins/ComposeAuthUI"
-                    "coil-integration" -> "plugins/CoilIntegration"
-                    "coil3-integration" -> "plugins/Coil3Integration"
-                    "imageloader-integration" -> "plugins/ImageLoaderIntegration"
-                    "serializer-moshi" -> "serializers/Moshi"
-                    "serializer-jackson" -> "serializers/Jackson"
-                    else -> ""
-                }
                 localDirectory.set(projectDir.resolve("src"))
-                remoteUrl.set(URL("https://github.com/supabase-community/supabase-kt/tree/master/$name/src"))
+                remoteUrl.set(URI("https://github.com/supabase-community/supabase-kt/tree/master/$name/src"))
                 remoteLineSuffix.set("#L")
             }
         }
---
+++ serializers/Jackson/README.md
@@ -0,0 +1,11 @@
+# Jackson Serializer
+
+# Installation
+
+### Add the module to your project
+
+```kotlin
+dependencies {
+    implementation("io.github.jan-tennert.supabase:serializer-jackson:VERSION")
+}
+```
