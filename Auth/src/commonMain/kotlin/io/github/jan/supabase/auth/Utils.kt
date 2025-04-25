78babfd4d0a0888f42de2ccbc41461e566c55e83
Jan Tennert <jan.m.tennert@gmail.com>
2025-04-25 14:24:08 +0200

    Fix exception on non-session hash parameters and only consume used parameters

--- Auth/src/commonMain/kotlin/io/github/jan/supabase/auth/Utils.kt
+++ Auth/src/commonMain/kotlin/io/github/jan/supabase/auth/Utils.kt
@@ -4,6 +4,7 @@ import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.annotations.SupabaseInternal
 import io.github.jan.supabase.auth.status.SessionSource
 import io.github.jan.supabase.auth.user.UserSession
+import io.github.jan.supabase.buildUrl
 import io.github.jan.supabase.logging.d
 import io.ktor.client.request.HttpRequestBuilder
 import kotlinx.coroutines.launch
@@ -11,7 +12,12 @@ import kotlinx.coroutines.launch
 @SupabaseInternal
 fun Auth.parseFragmentAndImportSession(fragment: String, onSessionSuccess: (UserSession) -> Unit = {}) {
     Auth.logger.d { "Parsing deeplink fragment $fragment" }
-    val session = parseSessionFromFragment(fragment)
+    val session = try {
+        parseSessionFromFragment(fragment)
+    } catch(e: IllegalArgumentException) {
+        Auth.logger.d(e) { "Received invalid session fragment. Ignoring." }
+        return
+    }
     this as AuthImpl
     authScope.launch {
         val user = retrieveUser(session.accessToken)
@@ -24,6 +30,22 @@ fun Auth.parseFragmentAndImportSession(fragment: String, onSessionSuccess: (User
 @SupabaseInternal
 fun HttpRequestBuilder.redirectTo(url: String) {
     this.url.parameters["redirect_to"] = url
+}
+
+internal fun consumeHashParameters(parameters: List<String>, url: String): String {
+    return buildUrl(url) {
+        fragment = fragment.split("&").filter {
+            it.split("=").first() !in parameters
+        }.joinToString("&")
+    }
+}
+
+internal fun consumeUrlParameter(parameters: List<String>, url: String): String {
+    return buildUrl(url) {
+        parameters.forEach { parameter ->
+            this.parameters.remove(parameter)
+        }
+    }
 }
 
 internal fun invalidArg(message: String): Nothing = throw IllegalArgumentException(message)
