d57486221babc0bb33907058a408d2e128e294a7
Jan Tennert <jan.m.tennert@gmail.com>
2025-04-26 16:52:27 +0200

    Rename test

--- Auth/src/commonTest/kotlin/UrlUtilsTest.kt
+++ Auth/src/commonTest/kotlin/UrlUtilsTest.kt
@@ -1,9 +1,12 @@
 import io.github.jan.supabase.auth.consumeHashParameters
 import io.github.jan.supabase.auth.consumeUrlParameter
+import io.github.jan.supabase.auth.redirectTo
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.url
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class UtilsTest {
+class UrlUtilsTest {
 
     @Test
     fun testConsumeHashParameters() {
@@ -18,6 +21,18 @@ class UtilsTest {
         val url = "https://example.com/?test=123&state=abc&code=xyz"
         val newUrl = consumeUrlParameter(listOf("test", "state"), url)
         val expectedUrl = "https://example.com/?code=xyz"
+        assertEquals(expectedUrl, newUrl)
+    }
+
+    @Test
+    fun testRedirectTo() {
+        val url = "https://example.com/"
+        val redirectTo = "https://redirect.com"
+        val newUrl = HttpRequestBuilder().apply {
+            url(url)
+            redirectTo(redirectTo)
+        }.url.toString()
+        val expectedUrl = "https://example.com/?redirect_to=https%3A%2F%2Fredirect.com"
         assertEquals(expectedUrl, newUrl)
     }
 
