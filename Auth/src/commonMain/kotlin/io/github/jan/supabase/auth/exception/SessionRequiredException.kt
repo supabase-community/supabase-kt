f6ce1c23be47538c649ab8cb8c591bfb02cb402c
Jan Tennert <jan.m.tennert@gmail.com>
2025-12-15 16:26:55 +0100

    Fix constructor parameter for SessionRequiredException

--- Auth/src/commonMain/kotlin/io/github/jan/supabase/auth/exception/SessionRequiredException.kt
+++ Auth/src/commonMain/kotlin/io/github/jan/supabase/auth/exception/SessionRequiredException.kt
@@ -5,7 +5,8 @@ import io.ktor.http.Url
 
 /**
  * An exception thrown when trying to perform a request that requires a valid session while no user is logged in.
+ * @param url The request url
  */
-class SessionRequiredException(val url: String): Exception("You need to be logged in to perform this request\nURL: ${StringMasking.maskUrl(
+class SessionRequiredException(url: String): Exception("You need to be logged in to perform this request\nURL: ${StringMasking.maskUrl(
     Url(url)
 )}")
